e07ca632d6e6038b6f5d8de4a7c04380b86673fc
pgacirane <42605843+pgacirane@users.noreply.github.com>
2025-07-11 09:58:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Smooth scrolling polyfill for older browsers (optional)
+// Smooth scrolling for in-page navigation
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener("click", function(e) {
     e.preventDefault();
